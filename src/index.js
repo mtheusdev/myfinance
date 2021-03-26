59b7af24d572356857243308c8007d38a3e2612f
Matheus Henrique <08990349931@edu.udesc.br>
2021-03-26 02:05:05 -0300

    finalizando aula 359, tudo ok !

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 const { GraphQLServer } = require('graphql-yoga')
+const Binding = require('prisma-binding')
 const { prisma } = require('./generated/prisma-client')
 
+const binding = new Binding.Prisma({
+  typeDefs: `${__dirname}/generated/graphql-schema/prisma.graphql`,
+  endpoint: process.env.PRISMA_ENDPOINT
+})
+
 const resolvers = {
-  Query:{
+  Query: {
     user(parent, args, context, info){
-      //return prisma.user({id: args.id})
-      // return{
-      //   id: 'miaaaaaaaaau',
-      //   name: 'Docker'
-      // }
+      // return prisma.user({id: args.id})
+      // .then(() => {
+      //   console.log('User: ', user)
+      //   return user
+      // })
+      return binding.query.user({where: {id: args.id}}, info)
+       .then(user => {
+        console.log('User: ', user)
+        return user
+      })
     }
   }
 }
