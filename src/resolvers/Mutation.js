346f2ed21c7fc06965e86b0d1d5601a3bb23db75
Matheus Henrique <08990349931@edu.udesc.br>
2021-03-26 19:10:05 -0300

    finalizando login, aula 366 ok

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -3,12 +3,35 @@ const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = process.env.JWT_SECRET
 
+async function login(_, { email, password }, ctx, info){
+
+  const user = await ctx.db.query.user({where: { email }})
+  if(!user){
+    throw new Error('Invalid credentials!')
+  }
+
+  const valid = await bcrypt.compare(password, user.password)
+  if(!valid){
+    throw new Error('Invalid credentials!')
+  }
+
+  const token = jwt.sign({userId: user.id}, JWT_SECRET, { expiresIn: '2h' })
+
+  return {
+    token,
+    user
+  }
+  
+}
+
+
+
 async function signup(_,args,ctx, info){
 
   const password = await bcrypt.hash(args.password, 10)
   const user = await ctx.db.mutation.createUser({data: {...args, password}})
   const token = jwt.sign({userId: user.id}, JWT_SECRET, { expiresIn: '2h' })
-  
+
   return {
     token,
     user
@@ -17,5 +40,6 @@ async function signup(_,args,ctx, info){
 }
 
 module.exports = {
-  signup
+  signup,
+  login
 }
